006c2093a8381d5319b2a5b5197ccd3bea654375
hetbhagatji09 <hetbhagatji09@gmail.com>
2024-06-27 20:25:10 +0530

    EDA file

--- src/exception.py
+++ src/exception.py
@@ -1,20 +1,20 @@
 import sys
-import logging
-def error_message_detail(error,error_detail:sys):
-    _,_,exc_tb=error_detail.exc_info()
-    file_name=exc_tb.tb_frame.f_code.co_filename
-    line_no=exc_tb.tb_lineno
-    err_mess=str(error)
-    error_message="Error occured in python script name[{0}] line number [{1}] error message[{2}]".format(file_name, line_no, err_mess)
+from src.logger import logging
 
+def error_message_detail(error, error_detail: sys):
+    _, _, exc_tb = error_detail.exc_info()
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    line_no = exc_tb.tb_lineno
+    err_mess = str(error)
+    error_message = "Error occured in python script name[{0}] line number [{1}] error message[{2}]".format(file_name, line_no, err_mess)
     return error_message
 
-
 class customException(Exception):
-    def __init__(self,error_message,error_detail:sys):
+    def __init__(self, error_message, error_detail: sys):
         super().__init__(error_message)
-        self.error_message=error_message_detail(error_message,error_detail=error_detail)
+        self.error_message = error_message_detail(error_message, error_detail=error_detail)
 
     def __str__(self):
         return self.error_message
+
 
